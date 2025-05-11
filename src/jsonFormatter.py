8161e9c8b72c45862f7581f1ffea4069ff82c299
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-05-10 21:14:27 -0500

    Squashed commit of the following:
    commit 00c93e408543c525ebf453eba79b2872fdaea0e8
    Author: AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
    Date:   Sat May 10 04:31:44 2025 -0500
    
        main gameplay loop finished

--- src/jsonFormatter.py
+++ src/jsonFormatter.py
@@ -4,22 +4,22 @@ import discord
 def formatEmbed(gameData : dict, i : discord.Interaction) -> discord.Embed:
     '''returns a formatted embed of a turn'''
     print(gameData)
-    playerName = i.user.display_name
+    username = i.user.display_name
     match gameData.get("outcome"):
         case "DEALER_WINS":
             title = "Dealer Wins"
         case "PLAYER_WINS":
-            title = f"{playerName} Wins"
+            title = f"{username} Wins"
         case "PUSH":
             title = "Draw, No One Wins"
         case __:
-            title = "Unresolved Game"
+            title = "Blackjack"
     embedVar = discord.Embed(title=title, color=0x00ff00)   
     embedVar.add_field(name="Dealer: ", value=f"Cards: {gameData.get("dealerCards")} | Value: {str(gameData.get("dealerValue"))}")
     for i in range(2): # adds blank spaces 
         embedVar.add_field(name='\u200b', value='\u200b', inline=False)
-    embedVar.add_field(name=f"Player: {playerName}", value=f"Cards: {gameData.get("playerCards")} | Value: {str(gameData.get("playerValue"))}")
-    embedVar.set_footer(text=f"Balance: {str(gameData.get("balance"))} | Bet: {str(gameData.get("currentBet"))} | Cards Remaining: {str(gameData.get("cardsRemaining"))} ")
+    embedVar.add_field(name=f"Player: {username}", value=f"Cards: {gameData.get("playerCards")} | Value: {str(gameData.get("playerValue"))}")
+    embedVar.set_footer(text=f"Balance: {str(gameData.get("balance"))} | Bet: {str(gameData.get("currentBet"))} | Cards Remaining: {str(gameData.get("cardsRemaining"))}")
     
     return embedVar
 
