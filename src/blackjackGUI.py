d3e8a9af49952c4b0f37c48c0ecc9c5a19fd0121
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-05-11 03:52:28 -0500

    added last and first page buttons

--- src/blackjackGUI.py
+++ src/blackjackGUI.py
@@ -5,12 +5,22 @@ from GameBoard import GameBoard
 from jsonFormatter import formatEmbed, formatSessionsEmbed
 
 class SessionList(discord.ui.View):
+    ''' Parameters
+        -----------
+    sesList: :class:`list`
+        dictionary of list of all sessions in a dictionary format   
+    curStartIndex: :class:`int`
+        first index that's at the top of the page 
+    entryRange: :class:`int`
+        how many entries should be in each page
+            
+        '''
     sesList : list[dict] = None
     listSize : int = 0
     curStartIndex : int = 0
     entryRange : int = 5
     
-    def __init__(self, sesList : list[dict], curStartIndex : int = 0, entryRange : int = 5, timeout = 180):
+    def __init__(self, sesList : list[dict], curStartIndex : int = 0, entryRange : int = 5, timeout = 300):
         super().__init__(timeout=timeout)
         self.listSize = sesList.__len__()
         self.sesList = sesList
@@ -23,7 +33,9 @@ class SessionList(discord.ui.View):
             prevButton : discord.Button= [x for x in self.children if x.custom_id == "prev"][0]
             prevButton.disabled = True
             
-        
+    @discord.ui.button(label="First Page", row=0, style=discord.ButtonStyle.primary)
+    async def firstpg_callback(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.edit_message(view=SessionList(sesList=self.sesList,entryRange=self.entryRange,curStartIndex=0),embed=formatSessionsEmbed(sessions=self.sesList, startIndex=0, entryRange=self.entryRange))   
         
     @discord.ui.button(label="Previous", row=0, style=discord.ButtonStyle.primary, custom_id="prev")
     async def prev_callback(self, interaction: discord.Interaction, button: Button):
@@ -34,7 +46,11 @@ class SessionList(discord.ui.View):
     async def next_callback(self, interaction: discord.Interaction, button: Button):
         self.curStartIndex += self.entryRange
         await interaction.response.edit_message(view=SessionList(sesList=self.sesList,entryRange=self.entryRange,curStartIndex=self.curStartIndex),embed=formatSessionsEmbed(sessions=self.sesList, startIndex=self.curStartIndex, entryRange=self.entryRange))
-
+        
+    @discord.ui.button(label="Last Page", row=0, style=discord.ButtonStyle.primary)
+    async def lastpg_callback(self, interaction: discord.Interaction, button: Button):
+        lastIndex =  self.listSize - (self.listSize % self.entryRange)
+        await interaction.response.edit_message(view=SessionList(sesList=self.sesList,entryRange=self.entryRange,curStartIndex=lastIndex),embed=formatSessionsEmbed(sessions=self.sesList, startIndex=lastIndex, entryRange=self.entryRange))   
         
         
 
